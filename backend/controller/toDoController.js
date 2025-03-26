ca65cd484706f1e64bd45cb2d472214619d61ae1
Satheeshkumar <satheeshkumaredu.gmail.com>
2025-03-26 23:44:20 +0530

    Updated Get Put Delete controllers, so that it interacts with DB

--- backend/controller/toDoController.js
+++ backend/controller/toDoController.js
@@ -2,10 +2,22 @@ const asyncHanlder = require('express-async-handler');
 const todo = require("../models/toDoModels")
 
 const get = asyncHanlder(async (request,response) => {
-    response.set(200).json({message: 'get'})
+    const todoObj = await todo.find()
+    response.set(200).json(todoObj)
 })
 const update = asyncHanlder(async (request,response) => {
-    response.set(200).json({message: 'update'})
+    const todoObj = await todo.findById(request.params.id)
+
+    if(!todoObj){
+        response.set(400)
+        throw new Error("Todo item not found")
+    }
+
+    const updatedTodo = await todo.findByIdAndUpdate(request.params.id,request.body,{
+        new: true, //will return updated document, if this tag is not included, original doc before update will be returned
+    })
+
+    response.set(200).json(updatedTodo)
 })
 const set = asyncHanlder(async (request,response) => {
     console.log(request.body)
@@ -19,7 +31,19 @@ const set = asyncHanlder(async (request,response) => {
     response.set(200).json(data)
 })
 const deleteToDo = asyncHanlder(async (request,response) => {
-    response.set(200).json({message: 'delete'})
+    const todoObj = await todo.findById(request.params.id)
+
+    if(!todoObj){
+        response.set(400)
+        throw new Error("Todo item not found")
+    }
+
+    await todo.findByIdAndDelete(request.params.id)    // this functions take id as parameter and deletes the respective object in the DB
+
+    response.set(200).json({
+        'message' : 'deleted',
+        'id' : request.params.id
+    })
 })
 
 module.exports = {
